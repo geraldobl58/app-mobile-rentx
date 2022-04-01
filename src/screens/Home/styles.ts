84655020b202a10a9f8990b5b3bbc6c897c638d1
Geraldo Luiz <geraldobl58@gmail.com>
2022-04-01 18:17:40 -0300

    :sparkles: Creating global application theme

--- src/screens/Home/styles.ts
+++ src/screens/Home/styles.ts
@@ -4,6 +4,11 @@ export const Container = styled.View`
   flex: 1;
   align-items: center;
   justify-content: center;
+
+  background-color: ${({ theme }) => theme.color.background_primary};
 `;
 
-export const Title = styled.Text``;
+export const Title = styled.Text`
+  font-size: 24px;
+  font-family: ${({ theme }) => theme.fonts.secondary_600};
+`;
