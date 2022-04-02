5f27dd3f4fbca71cc189c684cd2cd3e9cd8f7df2
Geraldo Luiz <geraldobl58@gmail.com>
2022-04-02 18:12:06 -0300

    :sparkles: Creating about section

--- src/screens/CarDetails/styles.ts
+++ src/screens/CarDetails/styles.ts
@@ -1,5 +1,8 @@
 import styled from 'styled-components/native';
+
 import { getStatusBarHeight } from 'react-native-iphone-x-helper'
+
+import { RFValue } from 'react-native-responsive-fontsize';
 
 export const Container = styled.View`
   flex: 1;
@@ -19,4 +22,62 @@ export const Header = styled.View`
 
 export const CarImages = styled.View`
   margin-top: ${getStatusBarHeight() + 32}px;
+`
+
+export const Content = styled.ScrollView.attrs({
+  contentContainerStyle: {
+    padding: 24,
+    alignItems: 'center'
+  },
+  showVerticalScrollIndicator: false
+})``
+
+export const Details = styled.View`
+  width: 100%;
+
+  flex-direction: row;
+  align-items: center;
+  justify-content: space-between;
+`
+
+export const Description = styled.View``
+
+export const Brand = styled.Text`
+  font-size: ${RFValue(10)}px;
+  color: ${({ theme }) => theme.colors.text_details};
+  font-family: ${({ theme }) => theme.fonts.secondary_500};
+
+  text-transform: uppercase;
+`
+
+export const Name = styled.Text`
+  font-size: ${RFValue(25)}px;
+  color: ${({ theme }) => theme.colors.title};
+  font-family: ${({ theme }) => theme.fonts.secondary_500};
+`
+
+export const Rent = styled.View``
+
+export const Period = styled.Text`
+  font-size: ${RFValue(25)}px;
+  color: ${({ theme }) => theme.colors.title};
+  font-family: ${({ theme }) => theme.fonts.secondary_500};
+
+  text-transform: uppercase;
+`
+
+export const Price = styled.Text`
+  font-size: ${RFValue(25)}px;
+  color: ${({ theme }) => theme.colors.main};
+  font-family: ${({ theme }) => theme.fonts.secondary_500};
+`
+
+export const About = styled.Text`
+  font-size: ${RFValue(15)}px;
+  color: ${({ theme }) => theme.colors.text};
+  font-family: ${({ theme }) => theme.fonts.secondary_400};
+
+  text-align: justify;
+
+  margin-top: 23px;
 `
