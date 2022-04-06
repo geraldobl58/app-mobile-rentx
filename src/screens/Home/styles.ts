e22ea93762aa5b8c8dfc5fa5fa1968c760e07300
Geraldo Luiz <geraldobl58@gmail.com>
2022-04-06 11:03:29 -0300

    :sparkles: Creating user gestures

--- src/screens/Home/styles.ts
+++ src/screens/Home/styles.ts
@@ -1,5 +1,4 @@
 import { FlatList, FlatListProps } from 'react-native'
-import { RectButton } from 'react-native-gesture-handler';
 
 import styled from 'styled-components/native';
 
@@ -40,19 +39,3 @@ export const CarList = styled(FlatList as new (props: FlatListProps<CarDTO>) => 
   },
   showVerticalScrollIndicator: false,
 })``;
-
-export const MyCarsButton = styled(RectButton)`
-  width: 60px;
-  height: 60px;
-
-  border-radius: 30px;
-
-  background-color: ${({ theme }) => theme.colors.main};
-
-  align-items: center;
-  justify-content: center;
-
-  position: absolute;
-  bottom: 13px;
-  right: 22px;
-`;
